99b3b132ef220d84178b3cf5b8231971ac7675e0
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-12-31 21:02:45 +0600

    middleware added

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -45,19 +45,25 @@ const Home = () => {
     <div className="max-w-7xl gap-14 mx-auto my-10">
       <div className="mb-10 flex justify-end gap-5">
         <button
-          className={`border px-3 py-2 rounded-full font-semibold ${activeClass} `}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            stock ? activeClass : null
+          } `}
           onClick={() => dispatch(toggle())}
         >
           In Stock
         </button>
         <button
-          className={`border px-3 py-2 rounded-full font-semibold`}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("amd") ? activeClass : null
+          } `}
           onClick={() => dispatch(toggleBrands("amd"))}
         >
           AMD
         </button>
         <button
-          className={`border px-3 py-2 rounded-full font-semibold`}
+          className={`border px-3 py-2 rounded-full font-semibold ${
+            brands.includes("intel") ? activeClass : null
+          } `}
           onClick={() => dispatch(toggleBrands("intel"))}
         >
           Intel
