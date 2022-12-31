3c7b56eb57ba0bcc5c7dc8ad1c13018142a1f6a3
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-12-31 12:27:27 +0600

    so far so good

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main/Main";
+import Home from "../pages/Main/Home";
+import About from "../pages/Main/About";
+import TopRated from "../pages/Main/About";
+import Cart from "../pages/Main/Cart";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      { path: "top-rated", element: <TopRated /> },
+      { path: "cart", element: <Cart /> },
+    ],
   },
 ]);
 
