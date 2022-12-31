3b81da105a63ba0fd3394e5862346ce6ca009465
MuhammadBorhan <0317371981@student.wub.edu.bd>
2022-12-31 18:00:53 +0600

    cart route done

--- src/components/ProductCart.js
+++ src/components/ProductCart.js
@@ -1,9 +1,12 @@
 import { MdDeleteForever } from "react-icons/md";
 import { BiListPlus } from "react-icons/bi";
 import { useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../features/cart/cartSlice";
 
 const ProductCart = ({ product }) => {
   const { pathname } = useLocation();
+  const dispatch = useDispatch();
   return (
     <div className="shadow-lg relative rounded-3xl border p-3 flex flex-col text-indigo-900">
       {pathname.includes("cart") && (
@@ -29,7 +32,10 @@ const ProductCart = ({ product }) => {
       </div>
       <div className="flex gap-2 mt-5">
         {!pathname.includes("cart") && (
-          <button className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold">
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
             Add to cart
           </button>
         )}
--- src/pages/Main/Cart.js
+++ src/pages/Main/Cart.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ProductCart from "../../components/ProductCart";
 
 const Cart = () => {
-  return <div>This is cart page</div>;
+  const cart = useSelector((state) => state.cart.cart);
+  console.log(cart);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
+      {cart.map((product) => (
+        <ProductCart key={product._id} product={product} />
+      ))}
+    </div>
+  );
 };
 
 export default Cart;
